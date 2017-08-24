faa9fe0ef0c14b8e99a675f1a3f34f92355d05de
kpicaza <kpicaza.sound@gmail.com>
2017-08-24 19:31:09 +0200

    replace go or cant buttons

--- src/plan/infrastructure/client/assist-to-plan.client.js
+++ src/plan/infrastructure/client/assist-to-plan.client.js
@@ -7,15 +7,43 @@
   var assistButton = $('.assist-to-plan');
   var cantAssistButton = $('.cant-assist');
 
+  var bindActions = function () {
+    assistButton = $('.assist-to-plan');
+    cantAssistButton = $('.cant-assist');
+    assistButton.unbind();
+    cantAssistButton.unbind();
+    assistButton.bind('click', assist);
+    cantAssistButton.bind('click', cantAssist);
+  };
+
+  var drawButton = function (button, type, planId, venueId) {
+    var buttonTexts = {
+      'cant-assist': ';-C Finally, i can\'t',
+      'assist-to-plan': 'I\'m go!!'
+    };
+
+    var htmlButton = '<button class="' + type
+      + ' mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" data-plan-id="'
+      + planId + '" data-venue-id="' + venueId + '">' + buttonTexts[type]
+      + '</button>';
+
+    button.replaceWith(htmlButton);
+    componentHandler.upgradeAllRegistered();
+    bindActions();
+  };
+
   var assist = function (e) {
     e.preventDefault();
 
     var button = $(e.target).closest('button');
     var assistantSpan = button.closest('div').find('.assistants');
+    var planId = button.data('plan-id');
+    var venueId = button.data('venue-id');
 
-    ajaxFunctions.ajaxRequest('POST', apiUrl + button.data('plan-id') + '/assistants', {
-      venueId: button.data('venue-id')
+    ajaxFunctions.ajaxRequest('POST', apiUrl + planId + '/assistants', {
+      venueId: venueId
     }, function (data) {
+      drawButton(button, 'cant-assist', planId, venueId);
       assistantSpan.text(data.nbrAssistants + ' ');
     }, function (e) {
       console.error(e, apiUrl);
@@ -28,10 +56,13 @@
 
     var button = $(e.target).closest('button');
     var assistantSpan = button.closest('div').find('.assistants');
+    var planId = button.data('plan-id');
+    var venueId = button.data('venue-id');
 
-    ajaxFunctions.ajaxRequest('DELETE', apiUrl + button.data('plan-id') + '/assistants', {
-      venueId: button.data('venue-id')
+    ajaxFunctions.ajaxRequest('DELETE', apiUrl + planId + '/assistants', {
+      venueId: venueId
     }, function (data) {
+      drawButton(button, 'assist-to-plan', planId, venueId);
       assistantSpan.text(data.nbrAssistants + ' ');
     }, function (e) {
       console.error(e, apiUrl);
@@ -40,14 +71,7 @@
   };
 
   body.bind('SearchWasMade', function () {
-
-    assistButton = $('.assist-to-plan');
-    cantAssistButton = $('.cant-assist');
-
-    assistButton.unbind();
-    cantAssistButton.unbind();
-    assistButton.bind('click', assist);
-    cantAssistButton.bind('click', cantAssist);
+    bindActions();
   });
 
 })();
